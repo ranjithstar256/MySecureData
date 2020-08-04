308a130342ad23b8831d4be48ffd28cbcf36cca0
ranji <ranjithstar256@gmail.com>
2020-08-04 15:20:23 +0530

    added decrypt code

--- app/src/main/java/com/am/mysecuredata/MainActivity.java
+++ app/src/main/java/com/am/mysecuredata/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -17,6 +19,7 @@ import android.widget.Toast;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -41,8 +44,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView=findViewById(R.id.imageView);
 
-        myDir= new File(Environment.getExternalStorageDirectory().toString()
-                        +"/savehere");
+        Intent  intent = new Intent(Intent.ACTION_PICK);
+        startActivity(intent);
+
+        myDir= new File(Environment.getExternalStorageDirectory().toString()+"/savehere");
+
         ActivityCompat.requestPermissions(MainActivity.this, new String[]
                 {Manifest.permission.READ_EXTERNAL_STORAGE,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE},
@@ -72,5 +78,29 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void decrypt(View view) {
+
+        File outputFileDec = new File(myDir,FILE_NAME_DEC);
+        File encFile = new File(myDir,FILE_NAME_ENC);
+        try {
+            MyEncrypter.decrypToFile(my_key,my_spec_key,
+                    new FileInputStream(encFile),new FileOutputStream(outputFileDec));;
+                    imageView.setImageURI(Uri.fromFile(outputFileDec));
+
+            Toast.makeText(this, "Decrypted!", Toast.LENGTH_SHORT).show();
+
+            //outputFileDec.delete();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+
     }
 }
