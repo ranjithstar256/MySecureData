a58197df324a3535a48b2c3cd48e2b1df449fc40
ranji <ranjithstar256@gmail.com>
2021-07-19 16:13:59 +0530

    coding fine tuning

--- app/src/main/java/com/am/mysecuredata/MainActivity.java
+++ app/src/main/java/com/am/mysecuredata/MainActivity.java
@@ -1,25 +1,19 @@
 package com.am.mysecuredata;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -31,79 +25,74 @@ import java.io.InputStream;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView;    File myDir;
-    private static final String FILE_NAME_ENC="tom_cruise_enc";
-    private static final String FILE_NAME_DEC="tom_cruise";
+    ImageView imageView;
+    File myDir;
+    private static final String FILE_NAME_ENC = "tom_cruise_enc";
+    private static final String FILE_NAME_DEC = "tom_cruise";
 
-    private String my_key="fl3kxbhiikdyl6og";
-    private String my_spec_key="anbdyhj35fj7s9db";
-
-    // example.txt
+    private String my_key = "fl3kxbhiikdyl6og";
+    private String my_spec_key = "anbdyhj35fj7s9db";
 
     String encrypted;
     String decrypted;
     EditText editText;
-    Drawable drawable ;
+    Drawable drawable;
     BitmapDrawable bitmapDrawable;
     Bitmap bitmap;
     ByteArrayOutputStream stream;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView=findViewById(R.id.imageView);
-        editText=findViewById(R.id.editTextTextPersonName);
+        imageView = findViewById(R.id.imageView);
+        editText = findViewById(R.id.editTextTextPersonName);
         File sourcePath = Environment.getExternalStorageDirectory();
         File path = new File(sourcePath + "/" + "savehere" + "/");
         path.mkdir();
 
 
-
-
         ActivityCompat.requestPermissions(MainActivity.this, new String[]
-                {Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        {Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
                 1);
     }
-    public void encrypt(View view) {
-        myDir= new File(Environment.getExternalStorageDirectory().toString()+"/savehere");
 
-        drawable = ContextCompat.getDrawable(MainActivity.this,R.drawable.tom_cruise);
-         bitmapDrawable= (BitmapDrawable) drawable;
-         bitmap = bitmapDrawable.getBitmap();
+    public void encrypt(View view) {
+        myDir = new File(Environment.getExternalStorageDirectory().toString() + "/savehere");
+
+        drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.tom_cruise);
+        bitmapDrawable = (BitmapDrawable) drawable;
+        bitmap = bitmapDrawable.getBitmap();
         stream = new ByteArrayOutputStream();
         // example.txt
 
-        InputStream  inputStream = new ByteArrayInputStream(stream.toByteArray());
-        File outputFileEnc = new File(myDir,FILE_NAME_ENC);
-        try{
-            MyEncrypter.encrypToFile(my_key,my_spec_key,inputStream,new FileOutputStream(outputFileEnc));
+        InputStream inputStream = new ByteArrayInputStream(stream.toByteArray());
+        File outputFileEnc = new File(myDir, FILE_NAME_ENC);
+        try {
+            MyEncrypter.encrypToFile(my_key, my_spec_key, inputStream, new FileOutputStream(outputFileEnc));
             Toast.makeText(this, "Encrypted!", Toast.LENGTH_SHORT).show();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException | InvalidAlgorithmParameterException | InvalidKeyException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public void decrypt(View view) {
-        myDir= new File(Environment.getExternalStorageDirectory().toString()+"/savehere");
 
-        File outputFileDec = new File(myDir,FILE_NAME_DEC);
-        File encFile = new File(myDir,FILE_NAME_ENC);
+    public void decrypt(View view) {
+        myDir = new File(Environment.getExternalStorageDirectory().toString() + "/savehere");
+
+        File outputFileDec = new File(myDir, FILE_NAME_DEC);
+        File encFile = new File(myDir, FILE_NAME_ENC);
         try {
-            MyEncrypter.decrypToFile(my_key,my_spec_key,
-                    new FileInputStream(encFile),new FileOutputStream(outputFileDec));;
+            MyEncrypter.decrypToFile(my_key, my_spec_key,
+                    new FileInputStream(encFile), new FileOutputStream(outputFileDec));
+            ;
             try (FileOutputStream out = new FileOutputStream(outputFileDec)) {
                 bitmap.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
                 // PNG is a lossless format, the compression factor (100) is ignored
@@ -111,21 +100,13 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-                    imageView.setImageURI(Uri.fromFile(outputFileDec));
+            imageView.setImageURI(Uri.fromFile(outputFileDec));
 
             Toast.makeText(this, "Decrypted!", Toast.LENGTH_SHORT).show();
 
             //outputFileDec.delete();
 
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
             e.printStackTrace();
         }
 
@@ -135,13 +116,13 @@ public class MainActivity extends AppCompatActivity {
         String d = editText.getText().toString();
 
 
-        encrypted = MyEncrypter.encrypt(d,MainActivity.this);
-        Toast.makeText(this, ""+encrypted, Toast.LENGTH_SHORT).show();
+        encrypted = MyEncrypter.encrypt(d, MainActivity.this);
+        Toast.makeText(this, "" + encrypted, Toast.LENGTH_SHORT).show();
 
     }
+
     public void decStr(View view) throws NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
         decrypted = MyEncrypter.decrypt(encrypted);
-        Toast.makeText(this, ""+decrypted, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "" + decrypted, Toast.LENGTH_SHORT).show();
     }
 }
-    //}
--- app/src/main/java/com/am/mysecuredata/MyEncrypter.java
+++ app/src/main/java/com/am/mysecuredata/MyEncrypter.java
@@ -3,91 +3,71 @@ package com.am.mysecuredata;
 import android.content.Context;
 import android.util.Base64;
 import android.util.Log;
-import android.widget.Toast;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.InvalidParameterSpecException;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class MyEncrypter {
-    private static final String ALGO_IMAGE_ENCRYPTOR="AES/CBC/PKCS5Padding";
+    private static final String ALGO_IMAGE_ENCRYPTOR = "AES/CBC/PKCS5Padding";
     private static final String ALGO_SECRET_KEY = "AES";
     private static final int READ_WRITE_HUFFER = 1024;
-    private static final String TAG ="234ERSJJ" ;
+    private static final String TAG = "234ERSJJ";
 
-    public static void encrypToFile(String keyStr,String specStr,InputStream in , OutputStream out)
-            throws IOException, NoSuchPaddingException,
-            NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException, InvalidKeyException {
+    public static void encrypToFile(String keyStr, String specStr, InputStream in, OutputStream out)
+            throws IOException {
         try {
-
             IvParameterSpec iv = new IvParameterSpec(specStr.getBytes("UTF-8"));
-
-            SecretKeySpec keySpec = new SecretKeySpec(keyStr.getBytes("UTF-8"),ALGO_SECRET_KEY);
-
+            SecretKeySpec keySpec = new SecretKeySpec(keyStr.getBytes("UTF-8"), ALGO_SECRET_KEY);
             Cipher c = Cipher.getInstance(ALGO_IMAGE_ENCRYPTOR);
-
-            c.init(Cipher.ENCRYPT_MODE,keySpec,iv);
-
-            out= new CipherOutputStream(out,c);
-            int count = 0 ;
+            c.init(Cipher.ENCRYPT_MODE, keySpec, iv);
+            out = new CipherOutputStream(out, c);
+            int count = 0;
             byte[] buffer = new byte[READ_WRITE_HUFFER];
-            while ((count=in.read(buffer))>0){
-                out.write(buffer,0,count);
+            while ((count = in.read(buffer)) > 0) {
+                out.write(buffer, 0, count);
             }
-            Log.d(TAG, "encrypToFile: "+out);
-        }
-        catch (Exception e){
-            Log.e(TAG, "encrypToFile: "+e.getMessage().toString()+"" );
-        }
-
-        finally {
+            Log.d(TAG, "encrypToFile: " + out);
+        } catch (Exception e) {
+            Log.e(TAG, "encrypToFile: " + e.getMessage().toString() + "");
+        } finally {
             out.close();
         }
     }
 
 
-
-    public static void decrypToFile(String keyStr,String specStr,InputStream in , OutputStream out)
-            throws IOException, NoSuchPaddingException,
-            NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException, InvalidKeyException {
+    public static void decrypToFile(String keyStr, String specStr, InputStream in, OutputStream out)
+            throws IOException {
         try {
 
             IvParameterSpec iv = new IvParameterSpec(specStr.getBytes("UTF-8"));
 
-            SecretKeySpec keySpec = new SecretKeySpec(keyStr.getBytes("UTF-8"),ALGO_SECRET_KEY);
+            SecretKeySpec keySpec = new SecretKeySpec(keyStr.getBytes("UTF-8"), ALGO_SECRET_KEY);
 
             Cipher c = Cipher.getInstance(ALGO_IMAGE_ENCRYPTOR);
 
-            c.init(Cipher.DECRYPT_MODE,keySpec,iv);
+            c.init(Cipher.DECRYPT_MODE, keySpec, iv);
 
-            out= new CipherOutputStream(out,c);
-            int count = 0 ;
+            out = new CipherOutputStream(out, c);
+            int count = 0;
             byte[] buffer = new byte[READ_WRITE_HUFFER];
-            while ((count=in.read(buffer))>0){
-                out.write(buffer,0,count);
+            while ((count = in.read(buffer)) > 0) {
+                out.write(buffer, 0, count);
             }
-            Log.d(TAG, "decrypToFile: "+out);
+            Log.d(TAG, "decrypToFile: " + out);
 
-        } catch (Exception e){
-            Log.e(TAG, "DEencrypToFile: "+e.getMessage().toString()+"" );
-        }finally {
+        } catch (Exception e) {
+            Log.e(TAG, "DEencrypToFile: " + e.getMessage().toString() + "");
+        } finally {
             out.close();
         }
     }
@@ -96,27 +76,22 @@ public class MyEncrypter {
     private static final String ALGORITHM = "Blowfish";
     private static final String MODE = "Blowfish/CBC/PKCS5Padding";
     private static final String IV = "abcdefgh";
-    private static final String KEY= "MyKey";
+    private static final String KEY = "MyKey";
 
-    public static  String encrypt(String value , Context c) throws
+    public static String encrypt(String value, Context c) throws
             NoSuchPaddingException, NoSuchAlgorithmException,
             InvalidAlgorithmParameterException, InvalidKeyException,
             BadPaddingException, IllegalBlockSizeException {
 
         SecretKeySpec secretKeySpec = new SecretKeySpec(KEY.getBytes(), ALGORITHM);
-
         Cipher cipher = Cipher.getInstance(MODE);
-
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(IV.getBytes()));
-        //cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(IV.getBytes()));
-
         byte[] values = cipher.doFinal(value.getBytes());
-        Toast.makeText(c, "Awesome ", Toast.LENGTH_SHORT).show();
-
         return Base64.encodeToString(values, Base64.DEFAULT);
     }
 
-    public static  String decrypt(String value) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    public static String decrypt(String value)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         byte[] values = Base64.decode(value, Base64.DEFAULT);
         SecretKeySpec secretKeySpec = new SecretKeySpec(KEY.getBytes(), ALGORITHM);
         Cipher cipher = Cipher.getInstance(MODE);
